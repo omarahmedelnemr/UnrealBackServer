15caac8a64a3987242a502806066e213f146e8c5
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-06-11 21:35:52 +0300

    update port

--- server.py
+++ server.py
@@ -128,4 +128,4 @@ def receive_data():
         return 'Data received successfully', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
